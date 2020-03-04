cceea193bc636c2387dda6415c90318a84f6dce7
leanbs <davin.yu1110@gmail.com>
2020-03-04 14:22:27 +0700

    add api

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import 'dotenv/config';
 import express from 'express';
 import models, { sequelize } from './models';
+import User from './controllers/User';
 
 const app = express();
 const eraseDatabaseOnSync = true;
 
-app.get('/', (req, res) => {
-  res.send('Hehe');
-})
+app.use('/users', User)
 
 sequelize.sync({ force: eraseDatabaseOnSync }).then(() => {
   app.listen(process.env.SERVER_PORT, () => {
