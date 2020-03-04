0fc62c28167473a41c5021061993d7f601ade95c
leanbs <davin.yu1110@gmail.com>
2020-03-04 15:04:41 +0700

    init crud

--- src/controllers/User.js
+++ src/controllers/User.js
@@ -1,9 +1,65 @@
-import { Router } from 'express';
+import { Router } from "express";
 
 const router = Router();
 
-router.get('/', async (req, res) => {
-  return res.send('Hello')
-})
+// get all data
+router.get("/", async (req, res) => {
+  const users = await req.context.models.User.findAll();
+
+  return res.send(users);
+});
+
+// create data
+router.post("/", async (req, res) => {
+  const { username } = req.body;
+
+  const user = await req.context.models.User.create({
+    username
+  });
+
+  return res.send(user);
+});
+
+// get single data
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+
+  const user = await req.context.models.User.findByPk(id);
+
+  return res.send(user);
+});
+
+// delete single data
+router.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+
+  const user = await req.context.models.User.destroy({
+    where: { id }
+  });
+
+  return res.send(user);
+});
+
+// update single data
+router.put("/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const user = await req.context.models.User.update(req.body, {
+      where: { id }
+    });
+
+    if (user[0] === 1) {
+      return res.send({
+        ok: true,
+        message: `user ${id} has successfully updated`
+      });
+    } else {
+      return res.send({ ok: false, message: `update failed` });
+    }
+  } catch (err) {
+    console.log(err);
+  }
+});
 
 export default router;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import 'dotenv/config';
 import express from 'express';
+import bodyParser from 'body-parser';
 import models, { sequelize } from './models';
 import User from './controllers/User';
 
 const app = express();
-const eraseDatabaseOnSync = true;
+const eraseDatabaseOnSync = false;
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use((req, res, next) => {
+  req.context = {
+    models,
+  };
+
+  next();
+});
 
 app.use('/users', User)
 
